6f3ff16713747f440b8596945644f5ab63f46240
Natalia Titova <bnmlucky@gmail.com>
2021-11-02 14:31:22 -0700

    Did an exercise on polymorphism and type casting and overriding methods in a subclass.

---
+++ src/chapter10/Banana.java
@@ -0,0 +1,14 @@
+package chapter10;
+
+public class Banana extends Fruit{
+    public Banana() {
+        setCalories(105);
+    }
+    public void peel() {
+        System.out.println("Banana has been peeled");
+    }
+    @Override
+    public void makeJuice() {
+        System.out.println("You can't make banana juice.");
+    }
+}
