42a7d702ac1e39427c3b99c98075576a8667298c
Natalia Titova <bnmlucky@gmail.com>
2021-11-01 18:18:11 -0700

    Looking at the concept of inheritance.

--- src/chapter9/Employee.java
+++ src/chapter9/Employee.java
@@ -2,6 +2,12 @@ package chapter9;
 
 public class Employee extends Person{
     private String employeeId;
+    private String title;
+
+    public Employee() {
+        super("Natalia");
+      System.out.println("In Employee default constructor");
+    }
 
     public String getEmployeeId() {
         return employeeId;
@@ -18,6 +24,14 @@ public class Employee extends Person{
     public void setTitle(String title) {
         this.title = title;
     }
-
-    private String title;
 }
+/*
+Superclass constructors run first
+A superclass's constructor is run before the subclass's constructor.
+Use 'super' to call specific constructors
+Explicit calls can be made to superclass's constructors from one of the subclass's constructors by using super.
+super calls must be first
+Explicit calls to the superclass's constructor must be the first statement in the subclass's constructor.
+Some constructor must be executed
+If the superclass does not have a default constructor, the subclass must explicitly call one of its other constructors.
+ */
--- src/chapter9/Person.java
+++ src/chapter9/Person.java
@@ -2,9 +2,13 @@ package chapter9;
 
 public class Person {
 
-    private String name;
+    public String name;
     private int age;
     private String gender;
+
+    public Person(String name){
+        System.out.println("In Person 2nd constructor. Name is set.");
+    }
 
     public String getName() {
         return name;
